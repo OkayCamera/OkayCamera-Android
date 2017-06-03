fe027bb0d1cd53c304c5d261657b918410d4d164
JianGuo Yang <lgzj2011@gmail.com>
2017-06-03 22:44:20 +0800

    make main Activity to full screen

--- app/src/main/java/com/okaycamera/okcamera/Main.kt
+++ app/src/main/java/com/okaycamera/okcamera/Main.kt
@@ -1,18 +1,30 @@
 package com.okaycamera.okcamera
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Main : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                             WindowManager.LayoutParams. FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
-        val tv = findViewById(R.id.sample_text) as TextView
-        tv.text = stringFromJNI()
+        initView();
+    }
+
+    /**
+     * init this Activity's views
+     */
+    fun initView() {
+        sample_text.text = stringFromJNI()
     }
 
     /**
@@ -22,7 +34,6 @@ class Main : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     companion object {
-
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
